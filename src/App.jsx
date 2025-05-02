5c837d7157953ae29ceedc7efcf4922b66aa445e
Esteban0010 <estebanpilchuman02@gmail.com>
2025-05-01 23:27:49 -0300

    paginado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { PaginationProvider } from './context/Pagination';
 import { NAVEGACION } from "./utils/const"
 import Home from './Pages/Home/Home';
 import Details from './Pages/Details/Details';
@@ -8,8 +9,8 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
+    <PaginationProvider>
     <BrowserRouter>
       <Routes>
         <Route element={<Home />} path={NAVEGACION.home} />
@@ -17,6 +18,7 @@ function App() {
         <Route element={<Landing />} path={NAVEGACION.landing} />
       </Routes>
     </BrowserRouter>
+    </PaginationProvider>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,29 @@
-// import useHeroes from "../../Hooks/
+import useHeroes from "../../Hooks/useHeroes";
 import Header from "../../Components/Header/Header"
-import React from 'react'
+import React,{useState} from 'react'
 import ContainerCardHeroe from '../../Components/containerCardHeroe';
 import ContainerCardMaps from '../../Components/containerCardMaps';
+import BannerHome from "../../Components/bannerHome/bannerHome";
+import BtnPaginado from "../../Components/btnPaginado/BtnPaginado";
+import { usePagination } from "../../context/Pagination";
 
 function Home() {
-  // const { heroes } = useHeroes()
+  const { heroes } = useHeroes();
+  const { currentPage} = usePagination();
+  const heroesPorPagina = 8;
 
+  // Cálculo de índices
+  const indexUltimoHeroe = currentPage * heroesPorPagina;
+  const indexPrimerHeroe = indexUltimoHeroe - heroesPorPagina;
+  const heroesActuales = heroes.slice(indexPrimerHeroe, indexUltimoHeroe);
   return (
     <div>
       <Header />
+      <BannerHome/>
       {/* <button onClick={irDetalles}>vamos detalles</button> */}
-      <ContainerCardHeroe />
-      <ContainerCardMaps />
-
+      <ContainerCardHeroe  heroes={heroesActuales}/>
+      {/* <ContainerCardMaps /> */}
+ <BtnPaginado elementosPorPagina={heroesPorPagina} />
     </div>
   )
 }

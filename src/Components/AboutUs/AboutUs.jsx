1ee03e7eb72b225dfcedd0727888916b9c6c47e1
Rodrigo Alberto Villablanca <138055427+shodrig0@users.noreply.github.com>
2025-05-02 03:30:39 -0300

    github api added

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom"
 import { NAVEGACION } from "../../utils/const"
 import Button from "../Button/Button"
+import Title from "../Title/Title"
+import ContainerAboutUs from "../Containers/ContainerAboutUs/ContainerAboutUs"
+import Navbar from "../Navbar/Navbar"
 
 const AboutUs = () => {
 
@@ -11,8 +14,13 @@ const AboutUs = () => {
 
     return (
         <>
-            <h2>prueba</h2>
-            <Button className={``} onClick={handleGoToHome} name={"Back"} />
+            <Navbar />
+            <Title title={"Development Team"} />
+            <ContainerAboutUs />
+            <Button
+                className="fixed bottom-6 right-6 z-50 bg-orange-400 hover:bg-orange-500 text-black font-bold py-2 px-4 rounded shadow-lg"
+                onClick={handleGoToHome}
+                name={"Back"} />
         </>
     )
 }

b21bf5511ca1181fd1d17ff4e95d1aea647a3827
Esteban0010 <estebanpilchuman02@gmail.com>
2025-05-04 19:44:42 -0300

    landin y card home resposive

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -6,13 +6,14 @@ import useHeroes from '../../Hooks/useHeroes';
 
 function Landing() {
   const navigate = useNavigate();
-  const {heroes }=useHeroes()
+  const { heroes } = useHeroes();
 
   const goToHome = () => {
-    navigate(NAVEGACION.home)
-  }
+    navigate(NAVEGACION.home);
+  };
+
   return (
-    <div className="relative h-screen w-full overflow-hidden">
+    <div className="relative h-screen w-full flex justify-center overflow-hidden">
       {/* Video de fondo */}
       <video
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
@@ -23,44 +24,56 @@ function Landing() {
         playsInline
       />
 
-      {/* Capa oscura para mejorar legibilidad */}
-      <div className="absolute inset-0  z-10"></div>
-
       {/* Contenido central */}
-      <div className="relative z-20 max-w-7xl mx-auto px-6 h-full flex flex-col md:flex-row items-center justify-center gap-10 text-white text-center md:text-left">
-        {/* Texto */}
-        <div className="md:w-1/2">
-          <h1 className="font-primary text-4xl md:text-5xl font-extrabold mb-4 leading-tight">
-            Welcome to OVERFAST
-          </h1>
-          <p className="text-xl md:text-3xl max-w-xl mb-4 leading-normal">
-            Discover all the <strong>heroes</strong>, explore the game's <strong>iconic maps</strong> and access <strong>strategic details</strong> all in one place!
-          </p>
-          <ul className="text-xl  space-y-4 mb-6">
-            <li className='flex'><Boxes className="w-10 h-10 text-yellow-400 mr-5" />Filter by role, game mode or region</li>
-            <li className='flex'><Star className="w-10 h-10 text-yellow-400 mr-5" />Save your favourites and access them quickly</li>
-            <li className='flex'><Search className="w-10 h-10 text-yellow-400 mr-5" />Find the information you need in seconds</li>
-          </ul>
+      <div className="relative z-20 max-w-6xl px-6 h-full flex flex-col md:flex-row items-center justify-around gap-10 text-white text-left">
 
-          {/* Botón debajo del texto */}
-          <button onClick={goToHome} className=" font-primary min-w-xs bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-6 rounded">
-            HOME
-          </button>
-        </div>
+{/* Logo para pantallas chicas (visible solo en sm) */}
+<div className="block md:hidden mb-4 flex justify-center w-full">
+  <img
+    src="/overwatch-logo.png"
+    alt="Overwatch Logo"
+    className="w-40 object-contain"
+  />
+</div>
 
-        {/* Imagen */}
-        <div className="md:w-1/2 flex justify-center">
-          <img
-            src="/overwatch-logo.png"
-            alt="Overwatch Logo"
-            className="w-64 md:w-80 lg:w-[28rem] object-contain"
-          />
-        </div>
-      </div>
-    </div>
+{/* Texto */}
+<div className="w-full h-full md:h-screen md:flex md:items-center md:justify-center">
+  <div className="w-full max-w-xl flex flex-col items-start">
+  <h1 className="font-primary md:text-center text-2xl md:text-3xl font-extrabold mb-4 leading-tight">
+    Welcome to OVERFAST
+  </h1>
+  <p className="text-base md:text-lg max-w-xl mb-4 leading-normal">
+    Discover all the <strong>heroes</strong>, explore the game's <strong>iconic maps</strong> and access <strong>strategic details</strong> all in one place!
+  </p>
+  <ul className="text-base md:text-lg space-y-4 mb-6">
+    <li className='flex items-center'><Boxes className="w-8 h-8 text-yellow-400 mr-3" />Filter by role, game mode or region</li>
+    <li className='flex items-center'><Star className="w-8 h-8 text-yellow-400 mr-3" />Save your favourites and access them quickly</li>
+    <li className='flex items-center'><Search className="w-8 h-8 text-yellow-400 mr-3" />Find the information you need in seconds</li>
+  </ul>
 
+  {/* Botón centrado */}
+  <div className="w-full flex sm:justify-center xl:justify-start">
+    <button
+      onClick={goToHome}
+      className="font-primary w-full md:w-full xl:w-[200px] bg-orange-600 hover:bg-orange-700 text-white font-semibold py-2 px-6 rounded"
+    >
+      HOME
+    </button>
+  </div>
+  </div>
+</div>
 
-  )
+{/* Logo para pantallas medianas en adelante */}
+<div className="hidden md:flex md:w-1/2 justify-end">
+  <img
+    src="/overwatch-logo.png"
+    alt="Overwatch Logo"
+    className="w-60 object-contain"
+  />
+</div>
+</div>
+</div>
+  );
 }
 
-export default Landing
+export default Landing;

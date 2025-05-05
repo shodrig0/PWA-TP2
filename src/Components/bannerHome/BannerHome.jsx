bc0c45ab1b7df8624456b935d6f0cfae71b8a2de
Rodrigo Alberto Villablanca <138055427+shodrig0@users.noreply.github.com>
2025-05-05 05:04:55 -0300

    LAS MAYUSCULAS ESTEBAN

--- src/Components/bannerHome/BannerHome.jsx
+++ src/Components/bannerHome/BannerHome.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function bannerHome() {
+function BannerHome() {
   return (
     <div className='flex justify-end w-full  xl:h-130 sm:x-full bg-black'>
-      <img src="https://www.gamercitymagazine.com/wp-content/uploads/2016/05/Overwatch-que-es-first-person-shooter-blizzard-entertainment.jpg" 
-      alt="" 
-className='h-full'      />
+      <img src="https://www.gamercitymagazine.com/wp-content/uploads/2016/05/Overwatch-que-es-first-person-shooter-blizzard-entertainment.jpg"
+        alt=""
+        className='h-full' />
     </div>
   )
 }
 
-export default bannerHome
+export default BannerHome

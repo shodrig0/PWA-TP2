866bacff691ce1378bd39fe4e2196a595fc192e0
Rodrigo Alberto Villablanca <138055427+shodrig0@users.noreply.github.com>
2025-05-05 04:45:47 -0300

    config vercel

--- src/Pages/Error/422.jsx
+++ src/Pages/Error/422.jsx
@@ -20,12 +20,12 @@ const PageValidationError = () => {
     }
 
     return (
-        <>
-            <h2>422 Error</h2>
-            <p>Oops! You need more parameters to be here :/</p>
-            <p>You're being redirected. Just wait a moment or click the button!</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white px-4">
+            <h2 className="text-6xl font-bold mb-4 text-orange-500">422 Error</h2>
+            <p className="text-lg mb-6">Oops! You need more parameters to be here :/</p>
+            <p className="text-lg mb-6">You're being redirected. Just wait a moment or click the button!</p>
             <Button onClick={handleRedirectionHome} className="btn-success bg-orange-400 hover:bg-orange-500 text-black font-bold py-2 px-4 rounded" name={"Back to Home!"} />
-        </>
+        </div>
     )
 }
 
